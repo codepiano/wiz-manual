d55774029ae43d0aacb329560a8a956b13584ae6
codepiano <codepiano.li@gmail.com>
2014-08-12 10:29:52 +0800

    增加打包脚本

---
+++ pkg.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo 使用的相对路径，会清空同级目录下的preview目录
+mkdir preview 
+rm -rf preview/*
+echo ----------------生成mac--------------------
+gitbook build Mac
+mv Mac/_book preview/mac
+echo ----------------生成windows----------------
+gitbook build PC
+mv PC/_book preview/windows
+echo ----------------生成web--------------------
+gitbook build web
+mv Web/_book preview/web
+echo ----------------生成android----------------
+gitbook build android
+mv android/_book preview/android
+echo ----------------生成ipad-------------------
+gitbook build iPad
+mv iPad/_book preview/ipad
+echo ----------------生成iphone-----------------
+gitbook build iPhone
+mv iPhone/_book preview/iPhone
+echo ----------------success--------------------
