70421d9ad9bbb012daea1c2d791f7a81db0a474c
codepiano <codepiano.li@gmail.com>
2014-08-15 09:58:44 +0800

    修改预览目录的名称

--- pkg.sh
+++ pkg.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-echo 使用的相对路径，会清空同级目录下的preview目录
-mkdir preview 
-rm -rf preview/*
-cp index.html preview/
+echo 使用的相对路径，会清空同级目录下的manual目录
+mkdir manual 
+rm -rf manual/*
+cp index.html manual/
 echo ----------------生成mac--------------------
 gitbook build Mac
-mv Mac/_book preview/mac
+mv Mac/_book manual/mac
 echo ----------------生成windows----------------
 gitbook build PC
-mv PC/_book preview/windows
+mv PC/_book manual/windows
 echo ----------------生成web--------------------
 gitbook build web
-mv Web/_book preview/web
+mv Web/_book manual/web
 echo ----------------生成android----------------
 gitbook build android
-mv android/_book preview/android
+mv android/_book manual/android
 echo ----------------生成ipad-------------------
 gitbook build iPad
-mv iPad/_book preview/ipad
+mv iPad/_book manual/ipad
 echo ----------------生成iphone-----------------
 gitbook build iPhone
-mv iPhone/_book preview/iphone
+mv iPhone/_book manual/iphone
 echo ----------------success--------------------
