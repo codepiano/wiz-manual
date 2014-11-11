16b2cab915c40fe126372041e6f7b7fe4180994e
codepiano <codepiano.li@gmail.com>
2014-11-11 17:16:07 +0800

    适配私有部署

--- pkg.sh
+++ pkg.sh
@@ -14,9 +14,6 @@ echo 使用的相对路径，会清空同级目录下的manual目录
 mkdir manual
 rm -rf manual/*
 cp index.html manual/
-echo ----------------生成mac--------------------
-gitbook build Mac
-mv Mac/_book manual/mac
 echo ----------------生成windows----------------
 gitbook build PC
 mv PC/_book manual/windows
@@ -26,12 +23,6 @@ mv Web/_book manual/web
 echo ----------------生成android----------------
 gitbook build android
 mv android/_book manual/android
-echo ----------------生成ipad-------------------
-gitbook build iPad
-mv iPad/_book manual/ipad
-echo ----------------生成iphone-----------------
-gitbook build iPhone
-mv iPhone/_book manual/iphone
 echo ----------------生成enterprise-----------------
 gitbook build enterprise
 mv enterprise/_book manual/enterprise
